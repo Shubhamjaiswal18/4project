51599e94aba1581911cbcfd4baea26b7ae1e1c93
Shubham <shubham.pebblemark@gmail.com>
2021-02-01 18:38:32 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+	crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+<title>4 project</title>
+</head>
+<body>
+	<section id="one">
+		<div class="container">
+			<div class="row text-center">
+				<div class="col-sm-12">
+					<h1 class="text-white display-4 my-5">#DAY3</h1>
+				</div>
+			</div>
+			<div class="row my-4 mx-auto">
+				<div class="col-sm-4  block1 mx-auto text-center">
+					<img class="img-fluid" src="img/image1.png">
+
+					<div class="col-sm-12 mt-3 ">
+						<input placeholder="Login" type="text" size="37" />
+					</div>
+					<div class="col-sm-12 mt-4">
+						<input placeholder="Password" type="text" size="37" />
+					</div>
+					<button class="btn my-5 sign">SIGN IN</button>
+					<div>
+						<button class="btn my-4 forgot">Forgot your Password?</button>
+					</div>
+
+
+				</div>
+				<div class="col-sm-4  block2 mx-auto text-center">
+					<img class="img-fluid" src="img/image1.png">
+					<div class="col-sm-12 mt-3">
+						<input placeholder="Login" type="text" size="37" />
+					</div>
+					<div class="col-sm-12 mt-4">
+						<input placeholder="Password" type="text" size="37" />
+					</div>
+					<button class="btn my-5 sign">SIGN IN</button>
+					<div>
+						<button class="btn my-4 forgot text-white ">Forgot your Password?</button>
+					</div>
+				</div>
+			</div>
+			
+			<h3 class= "text-white display-4 text-center space my-5">LOGIN SCREEN CONCEPTS</h3>
+		</div>
+	</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+		integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+		crossorigin="anonymous"></script>
+</body>
+</html>
